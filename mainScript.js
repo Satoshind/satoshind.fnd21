e89a0882294a2caeabc5d5b68f222d3835c49ef3
Satoshind <144880385+Satoshind@users.noreply.github.com>
2023-11-08 13:14:53 +0900

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -17,36 +17,35 @@ $(document).ready(function () {
 });
 
 const image = document.getElementById("anim_img");
-const SIZE = 10; // 枚数
+const numPict = 10; // 写真の枚数
 var tmp = {};
 loadImageToTmp();
 function loadImageToTmp() {
-  for (var i = 1; i <= SIZE; i++) {
-    const _i = i;
+  for (let i = 1; i <= numPict; i++) {
     const img = new Image();
-    tmp[_i] = null;
-    img.src = "anim/img" + _i + ".png"; // 連続するファイル名
+    tmp[i] = null;
+    img.src = "anim/img" + i + ".png"; 
     img.addEventListener("load", () => {
-      tmp[_i] = img;
+      tmp[i] = img;
     });
   }
 }
 
-const PX = 20; // 5px分の移動ごと画像を1枚進める
-const offset = $(document.getElementById("anim_img_box")).offset(); // 画像を入れるdiv要素(position:stickyでトップに来たら固定される)
+const pix = 20; 
+const offset = $(document.getElementById("anim_img_box")).offset();
 console.log(offset);
 $(window).scroll(function () {
   const y = $(window).scrollTop();
   const dy = y - offset.top;
-  if (offset.top < y && y < offset.top + SIZE * PX) {
+  if (offset.top < y && y < offset.top + numPict * pix) {
     $("anim_img_box").css("top", 0);
-    const i = Math.floor(dy / PX);
-    if (i > 0 || i < SIZE) {
+    const i = Math.floor(dy / pix);
+    if (i > 0 || i < numPict) {
       if (tmp[i].src) {
         image.src = tmp[i].src;
       }
     }
-  } else if (y >= offset.top + SIZE * PX) {
-    $("anim_img_box").css("top", "-" + (dy - SIZE * PX)); // スクロール分が終了したときに移動を始める
+  } else if (y >= offset.top + numPict * pix) {
+    $("anim_img_box").css("top", "-" + (dy - numPict * pix)); 
   }
 });
